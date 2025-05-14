296a26d9d417d9b572860a2f3ab7441b5137867a
DinhhSam <dinhhsam@gmail.com>
2025-05-14 23:36:12 +0700

    samvd/feat/test

---
+++ test.py
@@ -0,0 +1,11 @@
+n = int(input())
+a = list(map(int, input().split()))
+
+dp = [1] * n  # Khởi tạo mỗi phần tử có độ dài LIS là 1
+
+for i in range(n):
+    for j in range(i):
+        if a[j] < a[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(max(dp))
