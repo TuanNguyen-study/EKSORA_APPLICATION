893dea4b61c8a275fd0c98ba60af79c457c2c941
DinhhSam <dinhhsam@gmail.com>
2025-06-04 10:36:02 +0700

    sửa lỗi

--- EKSORA/app/_layout.tsx
+++ EKSORA/app/_layout.tsx
@@ -17,7 +17,7 @@ export default function Layout() {
         <Stack.Screen name="trip-detail" options={{ title: 'Trip Details' }} />
         <Stack.Screen name="loginEmail" options={{ title: 'login Email' }} />
         <Stack.Screen name="loginPhone" options={{ title: 'login Phone' }} />
-        <Stack.Screen name="home" options={{ title: 'home' }} />
+        {/* <Stack.Screen name="home" options={{ title: 'home' }} /> */}
         <Stack.Screen name="editFavorite" options={{ title: 'Edit Favorite',headerShown:false }} />
       </Stack>
     </Provider>
