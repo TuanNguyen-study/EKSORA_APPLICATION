23e53a2b366fac5dcef14463fc924cebb7c264c3
DinhhSam <dinhhsam@gmail.com>
2025-06-07 10:44:56 +0700

    hoàn thành giao diện trang home

--- EKSORA/API/server/serverCategories.jsx
+++ EKSORA/API/server/serverCategories.jsx
@@ -24,13 +24,25 @@ export const getTours = async () => {
 
 // API lấy danh sách các tour theo tỉnh
 export const getToursByLocation = async (province) => {
+    try {
+        const response = await AxiosInstance().get(`/api/categories/tours-by-location?province=${encodeURIComponent(province)}`);
+        return response; 
+    } catch (error) {
+        console.error('Lỗi khi lấy danh sách tour:', error);
+        throw error; 
+    }
+};
+
+// API lấy thông tin chi tiết của một tour
+export const getTourDetails = async (tourId) => {
   try {
-    const response = await AxiosInstance().get(`/api/categories/tours-by-location?province=${province}`);
-    return response;  
+    const response = await AxiosInstance().get(`/api/tours/${tourId}`);
+    return response; // Trả về dữ liệu chi tiết của tour
   } catch (error) {
-    console.error(`Lỗi khi lấy danh sách các tour tại tỉnh ${province}:`, error);
+    console.error('Lỗi khi lấy thông tin chi tiết tour:', error);
     throw error;
   }
 };
+
 
 
