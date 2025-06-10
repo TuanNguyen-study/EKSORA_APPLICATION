a38e3593a5f01822694211ef5b3bfab5547c520a
anhvo050605 <anhvo050605a@gmail.com>
2025-06-11 01:31:17 +0700

    tuananh/feat

--- EKSORA/API/services/servicesUser.jsx
+++ EKSORA/API/services/servicesUser.jsx
@@ -1,9 +1,9 @@
-import AxiosInstance from '../AxiosInstance';
+import AxiosInstance from './AxiosInstance';
 
 //API hiển thị user
 export const getUser = async () => {
   try {
-    const response = await AxiosInstance().get(`/api/profile`);
+    const response = await AxiosInstance.get(`/api/profile`);
     return response.data;
   } catch (error) {
     console.error('Lỗi khi hiển thị User:', error);
