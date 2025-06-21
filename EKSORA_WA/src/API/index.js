48a91f13e10a851430308f76c7b4465e51adbeb2
TuanNguyen-study <nhtuanstudyuser@gmail.com>
2025-06-21 11:23:17 +0700

    update WA

--- EKSORA_WA/src/API/index.js
+++ EKSORA_WA/src/API/index.js
@@ -1,22 +1,21 @@
+const API = process.env.REACT_APP_API;
+
 export const getNewProducts = () => {
-    return fetch("https://api.popeyes.vn/api/v1/categories/pop-cheese").then((res) => res.json());
-  };
-  
-  export const getRevenue = () => {
-    return fetch("https://api-8d22.onrender.com/cart/get_all_cart").then((res) => res.json());
-  };
-  
-  export const getInventory = () => {
-    return fetch("https://api.popeyes.vn/api/v1/products/specific?tag=best-seller&limit=100").then((res) => res.json());
-  };
-  
-  export const getPromo = () => {
-    return fetch("https://api.popeyes.vn/api/v1/products/BOGO%20DELI%20206-5936").then((res) => res.json());
-  };
-  export const getCustomers = () => {
-    return fetch("https://dummyjson.com/users").then((res) => res.json());
-  };
-  export const getComments = () => {
-    return fetch("https://dummyjson.com/comments").then((res) => res.json());
-  };
-  
+  return fetch(`${API}/api/categories`).then((res) => res.json());
+};
+
+export const getInventory = () => {
+  return fetch(`${API}/api/tours`).then((res) => res.json());
+};
+
+export const getPromo = () => {
+  return fetch(`${API}/api/vouchers`).then((res) => res.json());
+};
+
+export const getCustomers = () => {
+  return fetch(`${API}/categories`).then((res) => res.json());
+};
+
+export const getComments = () => {
+  return fetch(`${API}/categories`).then((res) => res.json());
+};
