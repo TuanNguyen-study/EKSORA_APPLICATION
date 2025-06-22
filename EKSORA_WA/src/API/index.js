6594445b36d6930a368a51ace71fdb0461e6a03f
DinhhSam <dinhhsam@gmail.com>
2025-06-22 18:43:45 +0700

    voches

--- EKSORA_WA/src/API/index.js
+++ EKSORA_WA/src/API/index.js
@@ -1,9 +1,30 @@
-const API = process.env.REACT_APP_API;
+const API = process.env.REACT_APP_API_URL;
 
 // Tours
 export const getInventory = () => {
   return fetch(`${API}/api/tours`).then((res) => res.json());
 };
+
+// Suppliers
+export const getSuppliers = () => {
+  return fetch(`${API}/api/suppliers`).then((res) => res.json());
+};
+
+// Customers (cái này chắc là /api/customers chứ không phải /categories ?)
+export const getCustomers = () => {
+  return fetch(`${API}/api/customers`).then((res) => res.json());
+};
+
+// Comments
+export const getComments = () => {
+  return fetch(`${API}/api/comments`).then((res) => res.json());
+};
+
+// Categories (nếu thực sự có)
+export const getNewProducts = () => {
+  return fetch(`${API}/api/categories`).then((res) => res.json());
+};
+
 
 // Vouchers
 export const getVoucher = () => {
@@ -24,14 +45,4 @@ export const deleteVoucher = (id) => {
   return fetch(`${API}/api/vouchers/${id}`, {
     method: "DELETE",
   }).then((res) => res.json());
-};
-
-// suppliersa
-export const getSuppliers = () => {
-  return fetch(`${API}/api/suppliers`).then((res) => res.json()); // Sửa lại endpoint phù hợp
-};
-
-// Comments
-export const getComments = () => {
-  return fetch(`${API}/api/comments`).then((res) => res.json()); // Sửa lại endpoint phù hợp
 };
