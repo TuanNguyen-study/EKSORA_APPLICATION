c79dbc7019b6386618a57ba8212e4e119ea715fd
DinhhSam <dinhhsam@gmail.com>
2025-07-25 08:18:20 +0700

    25/7

--- EKSORA/app/(stack)/acount/BookingCompleted/components/ContactInfoSection.jsx
+++ EKSORA/app/(stack)/acount/BookingCompleted/components/ContactInfoSection.jsx
@@ -11,7 +11,7 @@ import {
   ActivityIndicator, 
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+// import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { COLORS } from '../../../../../constants/colors';
 
 // -- COMPONENT FORM NHẬP LIỆU  --
