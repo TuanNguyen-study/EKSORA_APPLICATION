2a8a014caa30047b5e3a3a18068d9f45009514e1
Minhngocvo <cucvang2k5@gmail.com>
2025-08-15 23:08:33 +0700

    16.8

--- EKSORA/app/(stack)/MyOrder/BookingItem.jsx
+++ EKSORA/app/(stack)/MyOrder/BookingItem.jsx
@@ -17,7 +17,7 @@ const BookingItem = ({ item }) => {
     quantity_treEm,
   } = item;
 
-  const imageUrl = tour_id.image?.[0];
+  const imageUrl = tour_id?.image?.[0];
   const statusConfig = {
     paid: { text: 'ĐÃ XÁC NHẬN', color: COLORS.success, icon: 'check-circle' },
     pending: { text: 'ĐANG CHỜ XÁC NHẬN', color: COLORS.warning, icon: 'clock-time-eight' },
